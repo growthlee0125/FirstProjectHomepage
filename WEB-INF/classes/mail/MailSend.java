92e10806cc9431bd8ac8de7178d6b04eb3dce800
SJ.LEE <92525310+growthlee0125@users.noreply.github.com>
2022-01-06 10:03:31 +0900

    Update MailSend.java

--- WEB-INF/classes/mail/MailSend.java
+++ WEB-INF/classes/mail/MailSend.java
@@ -19,7 +19,7 @@ public class MailSend {
 
 		Properties prop = System.getProperties();
 		
-        // 로그인시 TLS를 사용할 것인지 설정
+        	// 로그인시 TLS를 사용할 것인지 설정
 		prop.put("mail.smtp.starttls.enable", "true");
         
 		// 이메일 발송을 처리해줄 SMTP서버
@@ -42,26 +42,28 @@ public class MailSend {
 			msg.setSentDate(new Date());
 
 			// 발송자를 지정한다. 발송자의 메일, 발송자명
-			msg.setFrom(new InternetAddress("project930125@gmail.com", "문의사항알림이"));
+			msg.setFrom(new InternetAddress("--@gmail.com", "문의사항알림이"));
 			
-            // 수신자의 메일을 생성한다.
-			InternetAddress to = new InternetAddress("newstart0125@naver.com");
+            		// 수신자의 메일을 생성한다.
+			InternetAddress to = new InternetAddress("--@naver.com");
 			
-            // Message 클래스의 setRecipient() 메소드를 사용하여 수신자를 설정한다. setRecipient() 메소드로 수신자, 참조,
+        		// Message 클래스의 setRecipient() 메소드를 사용하여 수신자를 설정한다. setRecipient() 메소드로 수신자, 참조,
 			// 숨은 참조 설정이 가능하다.
 			// Message.RecipientType.TO : 받는 사람
 			// Message.RecipientType.CC : 참조
 			// Message.RecipientType.BCC : 숨은 참조
 			msg.setRecipient(Message.RecipientType.TO, to);
 			
-            // 메일의 제목 지정
+            		// 메일의 제목 지정
 			msg.setSubject("문의사항이 도착했습니다.", "UTF-8");
 			
-            // Transport는 메일을 최종적으로 보내는 클래스로 메일을 보내는 부분이다.
+			// 메일의 내용 지정
 			msg.setText("문의자 이름 : "+mb.getName()+
 					    "\n\n 답변 받을 메일 : "+mb.getEmail()+
 					    "\n\n문의 메세지 : "+mb.getmessage()+
 					    "\n\n문의자 전화번호: "+mb.getPhone(),"UTF-8");
+			
+			// Transport는 메일을 최종적으로 보내는 클래스	
 			Transport.send(msg);
 
 		} catch (AddressException ae) {
